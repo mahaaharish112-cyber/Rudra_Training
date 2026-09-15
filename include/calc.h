5ea9ec1620b28dc50cd684112d5482504e144172
Harish <mahaaharish112@gmail.com>
2026-09-16 00:26:59 +0530

    dir src and include is Added

---
+++ include/calc.h
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+void add(int a,int b){
+	printf(" adding : %d + %d = %d ",a,b,(a+b));
+
+}
---
+++ src/prog.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "../include/calc.h"
+int main(){
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	add(a,b);
+	return 0;
+}
